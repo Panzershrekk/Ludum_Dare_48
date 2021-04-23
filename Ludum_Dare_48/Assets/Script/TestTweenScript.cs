615dcea008478b62945207261050366663f9768b
Panzershrekk <32634884+Panzershrekk@users.noreply.github.com>
2021-04-23 16:28:59 +0200

    Setting up Dotween

---
+++ Ludum_Dare_48/Assets/Script/TestTweenScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TestTweenScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMoveX(1, 1);
+    }
+}
